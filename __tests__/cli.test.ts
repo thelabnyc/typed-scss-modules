43699be9a6646cb8f83a9f40fead747251a0666a
msmx-mnakagawa <mnakagawa@mashmatrix.com>
2025-04-14 13:30:34 +0900

    test: enhance a test for `cli`

--- __tests__/cli.test.ts
+++ __tests__/cli.test.ts
@@ -1,27 +1,38 @@
 import { execSync } from "child_process";
+import { describeAllImplementations } from "./helpers";
 
-describe("cli", () => {
-  it("should run when no files are found", () => {
-    const result = execSync("npm run typed-scss-modules src").toString();
+describeAllImplementations((implementation) => {
+  describe("cli", () => {
+    it("should run when no files are found", () => {
+      const result = execSync("npm run typed-scss-modules src").toString();
 
-    expect(result).toContain("No files found.");
-  });
-
-  describe("examples", () => {
-    it("should run the basic example without errors", () => {
-      const result = execSync(
-        `npm run typed-scss-modules "examples/basic/**/*.scss" -- --includePaths examples/basic/core --aliases.~alias variables --banner '// example banner'`
-      ).toString();
-
-      expect(result).toContain("Found 3 files. Generating type definitions...");
+      expect(result).toContain("No files found.");
     });
 
-    it("should run the default-export example without errors", () => {
-      const result = execSync(
-        `npm run typed-scss-modules "examples/default-export/**/*.scss" -- --exportType default --nameFormat kebab --banner '// example banner'`
-      ).toString();
+    describe("examples", () => {
+      it("should run the basic example without errors", () => {
+        const result = execSync(
+          `npm run typed-scss-modules "examples/basic/**/*.scss" -- --includePaths examples/basic/core --aliases.~alias variables --banner '// example banner' --implementation ${implementation}`
+        ).toString();
 
-      expect(result).toContain("Found 1 file. Generating type definitions...");
+        expect(result).toContain(
+          "Found 3 files. Generating type definitions..."
+        );
+        expect(result).not.toContain("Can't find stylesheet to import.");
+        expect(result).not.toContain("File to import not found or unreadable");
+      });
+
+      it("should run the default-export example without errors", () => {
+        const result = execSync(
+          `npm run typed-scss-modules "examples/default-export/**/*.scss" -- --exportType default --nameFormat kebab --banner '// example banner' --implementation ${implementation}`
+        ).toString();
+
+        expect(result).toContain(
+          "Found 1 file. Generating type definitions..."
+        );
+        expect(result).not.toContain("Can't find stylesheet to import.");
+        expect(result).not.toContain("File to import not found or unreadable");
+      });
     });
   });
 });
