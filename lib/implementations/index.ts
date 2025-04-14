61676459edf7521457f8d3720d00b5ada893d762
msmx-mnakagawa <mnakagawa@mashmatrix.com>
2025-04-14 13:29:13 +0900

    refactor: remove `getImplementation()` and its test that become no longer used

--- lib/implementations/index.ts
+++ lib/implementations/index.ts
@@ -1,6 +1,3 @@
-import nodeSass from "node-sass";
-import sass from "sass";
-
 /**
  * A list of all possible SASS package implementations that can be used to
  * perform the compilation and parsing of the SASS files. The expectation is
@@ -9,8 +6,6 @@ import sass from "sass";
  */
 export const IMPLEMENTATIONS = ["node-sass", "sass"] as const;
 export type Implementations = (typeof IMPLEMENTATIONS)[number];
-
-type Implementation = typeof nodeSass | typeof sass;
 
 /**
  * Determine which default implementation to use by checking which packages
@@ -35,21 +30,4 @@ export const getDefaultImplementation = (
   }
 
   return pkg;
-};
-
-/**
- * Retrieve the desired implementation.
- *
- * @param implementation the desired implementation.
- */
-export const getImplementation = (
-  implementation?: Implementations
-): Implementation => {
-  if (implementation === "sass") {
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
-    return require("sass");
-  } else {
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
-    return require("node-sass");
-  }
 };
