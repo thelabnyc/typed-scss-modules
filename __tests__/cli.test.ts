0e589bb3347e1a5c2fba1d647f7506a7656f0bb9
msmx-mnakagawa <mnakagawa@mashmatrix.com>
2025-06-12 15:32:59 +0000

    fix: enhance example test cases

--- __tests__/cli.test.ts
+++ __tests__/cli.test.ts
@@ -8,14 +8,25 @@ describe("cli", () => {
     });
 
     describe("examples", () => {
+        const expectSuccessfulExecution = (
+            result: string,
+            expectedFiles: number,
+        ) => {
+            expect(result).toContain(
+                `Found ${expectedFiles} file${expectedFiles === 1 ? "" : "s"}. Generating type definitions...`,
+            );
+            expect(result).not.toContain("Can't find stylesheet to import.");
+            expect(result).not.toContain(
+                "File to import not found or unreadable",
+            );
+        };
+
         it("should run the basic example without errors", () => {
             const result = execSync(
                 `npm run typed-scss-modules "examples/basic/**/*.scss" -- --includePaths examples/basic/core --aliases.~alias variables --banner '// example banner'`,
             ).toString();
 
-            expect(result).toContain(
-                "Found 3 files. Generating type definitions...",
-            );
+            expectSuccessfulExecution(result, 3);
         });
 
         it("should run the default-export example without errors", () => {
@@ -23,9 +34,23 @@ describe("cli", () => {
                 `npm run typed-scss-modules "examples/default-export/**/*.scss" -- --exportType default --nameFormat kebab --banner '// example banner'`,
             ).toString();
 
-            expect(result).toContain(
-                "Found 1 file. Generating type definitions...",
-            );
+            expectSuccessfulExecution(result, 1);
+        });
+
+        it("should run the config-file example without errors", () => {
+            const result = execSync(
+                `cd examples/config-file && npm run typed-scss-modules "./**/*.scss"`,
+            ).toString();
+
+            expectSuccessfulExecution(result, 1);
+        });
+
+        it("should run the output-folder example without errors", () => {
+            const result = execSync(
+                `cd examples/output-folder && npm run typed-scss-modules "./**/*.scss"`,
+            ).toString();
+
+            expectSuccessfulExecution(result, 4);
         });
     });
 });
